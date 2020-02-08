55d20307355e1f0c549a05926c47e3a43d83b24a
Martin <martin.st8n@gmail.com>
2020-02-08 15:51:35 -0500

    first class main

---
+++ src/hello/MySweetProgram.java
@@ -0,0 +1,19 @@
+ package hello;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) { 
+		System.out.println("Hello there!!");
+
+	}
+
+}
+//comments
+//class - contains everything - has members
+//methods - do something
+//statements - Telling the computer to do something
+//properties - store something
+
+//access modifiers (public) - who can use?
+//static - No instance of class is needed
+//object - instance of a class
